5201309096692f2ad6d244d25bd96facdcc5aabd
Romina Velarde <rominapaola.velarde@gmail.com>
2020-08-06 23:32:23 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,61 @@
-# Add your code here
+require 'pry'
+
+def help 
+puts "I accept the following commands:"
+puts "- help : displays this help message"
+puts "- list : displays a list of songs you can play"
+puts "- play : lets you choose a song to play"
+puts "- exit : exits this program"
+ 
+end
+
+def list(songs)
+ 
+songs.each_with_index do |song, i|
+ 
+  puts "#{i + 1 }. #{song}"
+  
+ end 
+end
+
+def play(songs)
+    
+  puts "Please enter a song name or number:"
+  index = gets.strip
+  
+  if index.to_i < songs.length &&  index.to_i > 0 
+    puts "Playing #{songs[index.to_i-1]}"
+     
+   elsif songs.include?(index) 
+   
+   puts "Playing #{index}"
+else
+    puts "Invalid input, please try again"
+        
+    end 
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end 
+
+def run 
+puts "Please enter a command:"
+user = gets.strip
+
+while user != "exit"
+
+puts "Please enter a command:"
+user = gets.strip
+end
+binding.pry 
+if user < "list"
+  list(songs)
+elsif user == "play"
+  play(songs) 
+elsif user == "help" 
+  help  
+elsif user "exit" 
+  exit_jukebox
+  end 
+end 
